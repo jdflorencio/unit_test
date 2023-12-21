212d698bd4d50f8d2dfadca3e3d4647ae5826bb0
João Diego Florencio <jdflorencio@gmail.com>
2023-12-21 18:43:32 -0300

    sserções toBeGreaterThan e toBeGreaterThanOrEqual

--- __TEST__/grade.test.js
+++ __TEST__/grade.test.js
@@ -1,12 +1,25 @@
-const avarege = require('./../grade')
+const {average2, average3} = require('./../grade')
+
 
 describe("Checking studen grade", () => {
     it("should return true for grades 6, 7 and 5", () => {
-        expect(avarege(6,7,5)).toBeTruthy()
+        expect(average2(6,7,5)).toBeTruthy()
     })
 
     it("should return false for grades 3,5,4", () => {
-        expect(avarege(3,5,4)).toBeFalsy()
+        expect(average2(3,5,4)).toBeFalsy()
     })
 
 })
+
+
+describe("Checking studen grade", () => {
+    it("should check if the average of the grades 7,8 and 7 is greater 6", () => {
+        expect(average3(7,8,6)).toBeGreaterThan(6)
+    })
+
+    it("should check if the average of the grades 6 is greate then or equal to 6", () => {
+        expect(average3(6,6,6)).toBeGreaterThanOrEqual(6)
+    })
+})
+
--- grade.js
+++ grade.js
@@ -1,4 +1,11 @@
-module.exports = (grade1, grade2, grade3) => {
-    var average = (grade1 + grade2 + grade3) / 3;
+const average2 = (grade1, grade2, grade3) => {
+    let average = (grade1 + grade2 + grade3) / 3;
     return average >= 6;
 };
+
+ const average3 = (grade1, grade2, grade3) => {
+    return (grade1 + grade2 + grade3) / 3;
+  };
+
+
+  module.exports = {average2, average3}
