9faf61131c1b17e29dbb65127736f39873a63e45
João Diego Florencio <jdflorencio@gmail.com>
2023-12-21 10:53:41 -0300

    first

---
+++ excecao._test.js
@@ -0,0 +1,15 @@
+function compileAndroidCode() {
+    throw new Error('you are using the wrong JDK!');
+
+}
+
+test('compiling android goes as expected', () => {
+    expect(() => compileAndroidCode()).toThrow();
+    expect(() => compileAndroidCode().toThrow(Error));
+
+    //You can also use a string that must be contained in the error message or a regexp
+
+    expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK')
+    expect(() => compileAndroidCode)
+})
+
